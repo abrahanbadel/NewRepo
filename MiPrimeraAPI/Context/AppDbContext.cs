09392e3cc4c45ee104a793bd8d4aac90924d449f
abrahanbadel <abrahan.badel@cecar.edu.co>
2024-10-07 00:43:54 -0500

    Agregar archivos de proyecto.

---
+++ MiPrimeraAPI/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiPrimeraAPI.Models;
+
+namespace MiPrimeraAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Persona> Persona { get; set; }
+    }
+}
---
+++ MiPrimeraAPI/Models/Persona.cs
@@ -0,0 +1,10 @@
+﻿namespace MiPrimeraAPI.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+
+    }
+}
